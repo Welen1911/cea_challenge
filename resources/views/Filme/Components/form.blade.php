fc131a39db9c7a9777a063c9f0f0a86646d7fa0d
Welen1911 <welenalmeida2210@gmail.com>
2023-10-27 11:01:43 -0300

    Feat: form parcially styled

--- resources/views/Filme/Components/form.blade.php
+++ resources/views/Filme/Components/form.blade.php
@@ -1,7 +1,26 @@
     @csrf
-    Capa: <input type="file" name="image">
-    <input type="text" placeholder="Título" name="title" value="{{ $filme->title ?? old('title') }}">
-    <textarea name="description" id="" cols="30" rows="10" placeholder="Descrição">{{ $filme->description ?? old('description') }}</textarea>
-    <input type="number" placeholder="QTD" name="amount" value="{{ $filme->amount ?? old('amount') }}">
-    <input type="number" placeholder="Preço" name="price" value="{{ $filme->price ?? old('price') }}">
-    <button>Enviar</button>
+    <div class="form-group">
+        <label for="">Título</label>
+        <input type="text" class="form-control" id="" placeholder="ex: Interestrelar" name="title"
+            value="{{ $filme->title ?? old('title') }}">
+    </div>
+    <div class="form-group">
+        <label for="exampleInputPassword1">Descrição</label>
+        <textarea class="form-control" rows="3" name="description" placeholder="ex: interestelar é legal">{{ $filme->description ?? old('description') }}</textarea>
+    </div>
+    <div class="form-group">
+        <label for="">Quantidade:</label>
+        <input type="number" class="form-control" id="" name="amount" placeholder="ex: 0"
+            value="{{ $filme->amount ?? old('amount') }}">
+    </div>
+    <div class="form-group">
+        <label for="">Preço:</label>
+        <input type="number" class="form-control" id="" name="price" placeholder="ex: 0.00"
+            value="{{ $filme->price ?? old('price') }}">
+    </div>
+    <div class="form-group">
+        <label for="">Capa:</label>
+        <input type="file" id="" name="image">
+    </div>
+
+    <button type="submit" class="btn btn-dark">Submit</button>
--- resources/views/Filme/Edit.blade.php
+++ resources/views/Filme/Edit.blade.php
@@ -1,6 +1,6 @@
+@include('Filme.Components.header')
+
 <h1>Editar</h1>
-
-
 <form action="{{ route('update.film', $filme->id) }}" method="post" enctype="multipart/form-data">
     @method('PUT')
     @include('Filme.Components.form', ['filme' => $filme])
