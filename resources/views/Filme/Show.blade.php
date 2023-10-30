9b157ac85f715eba4098fea0646b00b0e26d448b
Welen1911 <welenalmeida2210@gmail.com>
2023-10-30 15:27:42 -0300

    Fix: image in show view

--- resources/views/Filme/Show.blade.php
+++ resources/views/Filme/Show.blade.php
@@ -3,7 +3,7 @@
 <section class="py-5">
     <div class="container px-4 px-lg-5 my-5">
         <div class="row gx-4 gx-lg-5 align-items-center">
-            <div class="col-md-6"><img class="card-img-top mb-5 mb-md-0" src="images/{{ $filme->image }}" alt="..." />
+            <div class="col-md-5"><img class="card-img-top mb-5 mb-md-0" src="/images/{{ $filme->image }}" alt="..." />
             </div>
             <div class="col-md-6">
                 <span>Quantidade: {{ $filme->amount }}</span>
