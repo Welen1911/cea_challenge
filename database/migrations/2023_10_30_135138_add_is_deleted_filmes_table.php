39615e522d6f93ca9594c6927697b3259664b97a
Welen1911 <welenalmeida2210@gmail.com>
2023-10-30 10:54:36 -0300

    Feat: isDeleted migration

---
+++ database/migrations/2023_10_30_135138_add_is_deleted_filmes_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('filmes', function (Blueprint $table) {
+            $table->boolean('isDeleted')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('filmes', function (Blueprint $table) {
+            $table->boolean('isDeleted')->nullable()
+            ->onDelete('cascade');
+        });
+    }
+};
